42bebd259343017cba467048eadc7a9d06ca0f36
SkyWallHacker <121939879+skywallhacker@users.noreply.github.com>
2023-03-30 04:03:10 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -40,11 +40,11 @@ function showSlides(n) {
 
     let curr = (slideIndex + 1).toString()
 
-    let totl  = (slides.length).toString()
-    
-    total.innerHTML = totl.padStart(2,0)
+    let totl = (slides.length).toString()
 
-    current.innerHTML = curr.padStart(2,0)
+    total.innerHTML = totl.padStart(2, 0)
+
+    current.innerHTML = curr.padStart(2, 0)
 
     slides[slideIndex].classList.remove('hide')
     slides[slideIndex].classList.add('fade')
@@ -95,4 +95,95 @@ title.forEach((item) => {
     }
 })
 
+let genBtns = document.querySelectorAll('#gender .calculating__choose-item')
+let inputs = document.querySelectorAll('.calculating__choose_medium input')
+let actBtns = document.querySelectorAll('.calculating__choose_big .calculating__choose-item')
+let resultView = document.querySelector('#result')
+
+let userData = {
+    gender: "woman"
+}
+
+genBtns.forEach(btn => {
+    btn.onclick = () => {
+        genBtns.forEach(el => el.classList.remove('calculating__choose-item_active'))
+
+        btn.classList.add('calculating__choose-item_active')
+
+        let gen = btn.getAttribute('data-gen')
+        userData.gender = gen
+
+    }
+})
+
+inputs.forEach(inp => {
+    inp.onkeyup = () => {
+        userData[inp.name] = inp.value
+    }
+})
+
+actBtns.forEach((btn => {
+    btn.onclick = () => {
+        actBtns.forEach(el => el.classList.remove('calculating__choose-item_active'))
+
+        btn.classList.add('calculating__choose-item_active')
+
+        let act = +btn.getAttribute('data-act')
+
+        inputs.forEach((elem) => {
+            if (elem.value === '') {
+                elem.classList.add('error', 'eror')
+            } else {
+                elem.classList.remove('error', 'eror')
+            }
+        })
+
+        if (userData.gender === 'woman') {
+            let BMR = 447.6 + (9.2 * userData.weight) + (3.1 * userData.height) - (4.3 * userData.age)
+
+            resultView.innerHTML = Math.round(BMR * act)
+        } else {
+            let BMR = 88.36 + (13.4 * userData.weight) + (4.8 * userData.height) - (5.7 * userData.age)
+
+            resultView.innerHTML = Math.round(BMR * act)
+        }
+    }
+}))
+
+let seconds = document.querySelector('#seconds')
+
+let minutes = document.querySelector('#minutes')
+
+let hours = document.querySelector('#hours')
+
+let days = document.querySelector('#days')
+
+let s = +seconds.innerHTML
+
+let m = +minutes.innerHTML
+
+let h = +hours.innerHTML
+
+let d = +days.innerHTML
+
+
+setInterval(() => {
+    s += 1
+    seconds.innerHTML = s
+    minutes.innerHTML = m
+    hours.innerHTML = h
+    days.innerHTML = d
+    if (s === 60) {
+        s = 0
+        m += 1
+        if (m === 60) {
+            m = 0
+            h += 1
+            if (h === 24) {
+                h = 0
+                d += 1
+            }
+        }
+    }
+}, 1000)
 
